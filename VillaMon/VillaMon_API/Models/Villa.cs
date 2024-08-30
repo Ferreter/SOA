5744aa90d22a8d4c441b157299d57020e8bb3c21
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2024-08-30 21:58:27 +0100

    :fire: Enhancing Dataset

--- VillaMon/VillaMon_API/Models/Villa.cs
+++ VillaMon/VillaMon_API/Models/Villa.cs
@@ -3,9 +3,15 @@
     public class VillaDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty; // Initialize with a default value
-
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
     
 }
